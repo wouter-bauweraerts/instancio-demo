54fd4bac8501ff6cf7997da3e43b6574970f51fc
Wouter Bauweraerts <wouter.bauweraerts@telenet.be>
2024-08-27 16:33:05 +0200

    commit initial setup

---
+++ src/test/java/com/example/wouterbauweraerts/instancio/demo/SetupTest.java
@@ -0,0 +1,12 @@
+package com.example.wouterbauweraerts.instancio.demo;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class SetupTest {
+    @Test
+    void testRuns() {
+        assertThat(true).isTrue();
+    }
+}
